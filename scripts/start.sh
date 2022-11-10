78f51f4bb0127cfd1837e673d6668d8075f831ed
patshone-gsl <65708894+patshone-gsl@users.noreply.github.com>
2022-11-10 13:53:11 +0000

    adds mailhog to local development environment

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 source ./scripts/init.sh
 
 echo "### Starting required containers (db=${CHOSEN_DATABASE})###"
-docker-compose -f ${COMPOSE_FILE} up -d php-fpm ${CHOSEN_DATABASE} nginx dgraph-zero dgraph-server redis
+docker-compose -f ${COMPOSE_FILE} up -d php-fpm ${CHOSEN_DATABASE} nginx dgraph-zero dgraph-server redis mailhog
